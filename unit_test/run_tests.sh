ff9b5ebbefcb8f2f72f329ef03e74b7ce53d6d8d
Ovoda <cdetourrispro@gmail.com>
2022-03-18 13:39:26 +0100

    bash script: clean a.out added

--- unit_test/run_tests.sh
+++ unit_test/run_tests.sh
@@ -7,23 +7,24 @@ do
     if [ $? -ne 0 ]
     then
         echo "Error on $f : This is not a valgrind error, your program might have leaks or invalid read/write"
-		exit 1
+        exit 1
     fi
-
+    
     clang++ -Wall -Werror -Wextra -std=c++98 -D NS=std -I ../ $f -I ./ && valgrind ./a.out > std 2>/dev/null
-	if [ $? -ne 0 ]
+    if [ $? -ne 0 ]
     then
         echo "Error on stl : that shouldn't be happening, please contact me (calide-n@student.42.fr)"
-		exit 1
+        exit 1
     fi
-
-
+    
+    
     diff ft std
     if [ $? -eq 0 ]
     then
         echo "$f passed"
-	else
-		echo "$f diff error : stl behaves differently"
+    else
+        echo "$f diff error : stl behaves differently"
     fi
-	rm -rf ft std
+    rm -rf ft std
+    rm -rf a.out
 done
